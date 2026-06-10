44da5dd4d5e19055de490b83c64aff7a92cf028b
2Emma-zhou <110969310+2Emma-zhou@users.noreply.github.com>
2026-06-10 10:02:31 +0800

    Update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/freezonex logo.svg',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://clouddoc.tier0.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
